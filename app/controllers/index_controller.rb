af5e59054329b1105be52d74d526b275e0db2cc4
Peyton Caseria <PeytonCaseria@gmail.com>
2016-01-10 19:56:31 -0700

    Another commit

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,62 @@
+class IndexController < ApplicationController
+  def show
+    @array = Array.find(params[:id])
+    if @books.special?
+render action: "Array" and return
+end
+
+#integrating array into new page randomizer function
+
+      Array.new(26) {[AA,BB,CC,DD,EE,FF,GG,HH,II,JJ,KK,LL,MM,NN,OO,PP,QQ,RR,SS,TT,UU,VV,WW,XX,YY,ZZ]}
+  end             ########1##2##3##4##5##6##7##8##9##10#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#
+render action: "Array.new"
+
+  def table
+    AA = AA.html.erb
+    BB = BB.html.erb
+    CC = CC.html.erb
+    DD = DD.html.erb
+    EE = EE.html.erb
+    FF = FF.html.erb
+    GG = GG.html.erb
+    HH = HH.html.erb
+    II = II.html.erb
+    JJ = JJ.html.erb
+    KK = KK.html.erb
+    LL = LL.html.erb
+    MM = MM.html.erb
+    NN = NN.html.erb
+    OO = OO.html.erb
+    PP = PP.html.erb
+    QQ = QQ.html.erb
+    RR = RR.html.erb
+    SS = SS.html.erb
+    TT = TT.html.erb
+    UU = UU.html.erb
+    VV = VV.html.erb
+    WW = WW.html.erb
+    XX = XX.html.erb
+    YY = YY.html.erb
+    ZZ = ZZ.html.erb
+ end
+
+  def view = viewArray( => [26])
+
+    viewArray = Selected
+
+    myArrayPrompt = 'AA' + 'BB' + 'CC' + 'DD' + 'EE' + 'FF' + 'GG' + 'HH' + 'II' + 'JJ' + 'KK' 'LL' + 'MM' + 'NN' + 'OO' + 'PP' + 'QQ' + 'RR' + 'SS' + 'TT' + 'UU' + 'VV' + 'WW' + 'XX' + 'YY' + 'ZZ'
+    if myArrayPrompt myArray
+    
+else
+
+ viewArray = Selected 
+return myArray.Prompt 
+
+else myArrayPrompt
+
+
+elsif Array(26)
+  
+
+  end
+end
